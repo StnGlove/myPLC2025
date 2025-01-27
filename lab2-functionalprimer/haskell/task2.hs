5c868681d350aac3ac60cec9786569fcef57b8e5
Stan Prichard <stanmp19@gmail.com>
2025-01-27 16:10:53 +0000

    1+1 code

--- lab2-functionalprimer/haskell/task2.hs
+++ lab2-functionalprimer/haskell/task2.hs
@@ -1,4 +1,3 @@
 main = do
-    putStrLn "Welcome to the programme. Please enter your name"
-    name <- getLine
-    putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
+    print ("1 + 1 = " ++ show(onePlusOne 1))
+onePlusOne x = x + 1
