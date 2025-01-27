d39c3cd4bde51bd9cdead9b55f0d2bc7d17a7e6d
Stan Prichard <stanmp19@gmail.com>
2025-01-27 16:42:09 +0000

    updated code

--- lab2-functionalprimer/haskell/task4.hs
+++ lab2-functionalprimer/haskell/task4.hs
@@ -4,7 +4,8 @@ ask prompt =
   putStrLn prompt
   line <- getLine
   if line == ""
-    then ask prompt
+    then ask (prompt ++ "!")
+    else if line == "quit" then print "quitting..."
     else putStrLn ("you said: " ++ reverse line)
 
 main :: IO ()
